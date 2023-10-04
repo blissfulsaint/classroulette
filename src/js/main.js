68f8af681f6b4829dada5d87176f98c6bd255502
Jaelee Hutchinson <hut21003@byui.edu>
2023-10-04 13:25:54 -0600

    button addition

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// change inside of the function later
+// pass is placholder for selecting students
+function onButtonClick() {
+    pass;
+}
+  
+// click button
+const button = document.querySelector('button');
+button.addEventListener('click', onButtonClick);
