bc4caadcc178d3e29ceb51a9b19a5d47b60de488
Kaleb Bradford <wannabdiogenes@gmail.com>
2023-10-11 13:18:02 -0600

    added comments for clarity

--- src/js/main.js
+++ src/js/main.js
@@ -3,25 +3,31 @@ import Student from './Student.js';
 
 loadHeaderNavFooter();
 
+// here to detect mobile devices
 function detectMob() {
     let mobile = false
     if ( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) )
     return (mobile);
   }
 
+// gettting information from folder "names", and using information
+// to create student cards
 async function fetchData() {
     let response = await fetch('../json/names.json');
     let data = await response.json();
     let students = data.students;
 
     let studentCards = new Array();
-    let i = 0;   
+    let i = 0;  
+
+    // if mobile device, create cards for all students, and include first names and image 
     if (detectMob() = true) {
         students.forEach(element => {
             studentCards[i] = new Student(element.fname, element.image);
             i++;
         })
     }
+    // else, print cards of all students, and include first name, last name, and image
     else {
     students.forEach(element => {
         studentCards[i] = new Student(element.fname, element.lname, element.image);
@@ -29,6 +35,7 @@ async function fetchData() {
     })
     }
 
+    // return the cards that we created
     return students;
 }
 
