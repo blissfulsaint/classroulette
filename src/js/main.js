73001dd5c4cdabd84bda5c98c0cab705a1bf9e7c
Kaleb Bradford <wannabdiogenes@gmail.com>
2023-10-11 13:14:49 -0600

    made only student fname only appear in mobile

--- src/js/main.js
+++ src/js/main.js
@@ -3,18 +3,31 @@ import Student from './Student.js';
 
 loadHeaderNavFooter();
 
+function detectMob() {
+    let mobile = false
+    if ( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) )
+    return (mobile);
+  }
+
 async function fetchData() {
     let response = await fetch('../json/names.json');
     let data = await response.json();
     let students = data.students;
 
     let studentCards = new Array();
-    let i = 0;
-
+    let i = 0;   
+    if (detectMob() = true) {
+        students.forEach(element => {
+            studentCards[i] = new Student(element.fname, element.image);
+            i++;
+        })
+    }
+    else {
     students.forEach(element => {
         studentCards[i] = new Student(element.fname, element.lname, element.image);
         i++;
     })
+    }
 
     return students;
 }
